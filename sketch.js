db05a38f2eac118b9f02a0aa659038c9c47f9983
shresh101 <65058657+shresh101@users.noreply.github.com>
2021-01-10 12:22:35 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var drone1;
+var missile1, bullet1;
+
+
+
+function setup() {
+  var canvas = createCanvas(displayWidth-40 , displayHeight - 40);
+  
+  engine = Engine.create();
+  world = engine.world;
+
+  drone1 = new Drone(400,100,50,50);
+  //Engine.run(engine);
+}
+
+function draw() {
+  background(255,255,0);  
+  Engine.update(engine);
+  //drawSprites();
+  drone1.display();
+}
+function moving() {
+  if(keyIsDown(37)){
+    drone1.x -= 10
+  }
+  if(keyIsDown(39)){
+    drone1.x += 10
+  }
+}
